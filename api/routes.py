182b7a3f86bf0645e22d0f197aabc7d6f02424da
Sannidhya Ray <sannidhya.ray2005@gmail.com>
2025-12-23 06:37:56 +0530

    routes fixed

--- api/routes.py
+++ api/routes.py
@@ -1,11 +1,15 @@
 from fastapi import APIRouter, UploadFile, File
+from core.image_detector import detect_image
 
 router = APIRouter()
 
 @router.post("/detect")
 async def detect(file: UploadFile = File(...)):
+    content = await file.read()
+
+    if file.content_type.startswith("image"):
+        return detect_image(content)
+
     return {
-        "filename": file.filename,
-        "content_type": file.content_type,
-        "message": "API is working"
+        "error": "Only image files supported for now"
     }
